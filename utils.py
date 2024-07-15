8d79f5294920a9d15413f6043f19e919814b140f
Xinyi Ying <yingxinyi18@nudt.edu.cn>
2024-07-15 10:14:32 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -67,7 +67,7 @@ def random_crop(img, mask, patch_size, pos_prob=None):
         mask = np.pad(mask, ((0, max(h, patch_size)-h),(0, max(w, patch_size)-w)), mode='constant')
         h, w = img.shape
 
-    cur_pos_prob = random.random()
+    pos_prob = random.random()
     if pos_prob == None or cur_prob > pos_prob or mask.max() == 0:
         h_start = random.randint(0, h - patch_size)
         w_start = random.randint(0, w - patch_size)
@@ -106,6 +106,8 @@ def get_img_norm_cfg(dataset_name, dataset_dir):
         img_norm_cfg = dict(mean=62.10432052612305, std=23.96998405456543)
     elif dataset_name == 'IRDST-real':   
         img_norm_cfg = {'mean': 101.54053497314453, 'std': 56.49856185913086}
+    elif dataset_name == 'LimitIRTSTD-track2':
+       	img_norm_cfg = {'mean': 64.21671295166016, 'std': 24.50885772705078}
     else:
         with open(dataset_dir + '/' + dataset_name +'/img_idx/train_' + dataset_name + '.txt', 'r') as f:
             train_list = f.read().splitlines()
